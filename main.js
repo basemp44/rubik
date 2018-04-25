475ba24abd40fa2decb779df8a7029d83296be5c
Víctor Macías <victor.maciaspalla@soprasteria.com>
2018-04-25 12:45:02 +0200

    feat: rotation Y axis

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+	let colors = {
+		U: "white",
+		D: "yellow",
+		L: "orange",
+		R: "red",
+		F: "green",
+		B: "blue",
+		black: "black"
+	}
+
+	populateLayerX(colors);
+	populateLayerY(colors);
+}
--- populate-rubik.js
+++ populate-rubik.js
@@ -13,58 +13,51 @@ function create(tag, classList, children=null) {
 }
 
 function populateLayerX(colors) {
-	getElementsByClass("layerX").forEach(layer => {
-		let xU = createXU(colors['U']);
-		let xD = createXD(colors['D']);
-		let xL = createXL(colors['L']);
-		let xR = createXR(colors['R']);
-		let xF = createXF(colors['F']);
-		let xB = createXB(colors['B']);
+	getElementsByClass("layerX").forEach((layer,i) => {
+		let xU = createXU(colors.U);
+		let xD = createXD(colors.D);
+		let xL = createXL(i == 0 ? colors.L: colors.black);
+		let xR = createXR(i == 2 ? colors.R: colors.black);
+		let xF = createXF(colors.F);
+		let xB = createXB(colors.B);
 
 		[xU,xD,xL,xR,xF,xB].forEach(e => layer.appendChild(e))
 	})
 
-	function createXU(color) {
-		let children = Array.from({length: 3}, () => create("div", `tile ${color}`));
-		return create("div", "tilegroup tilegroup-1-3 x-up", children);
-	}
+	function createXU(color) { return createXCustom(3, color, 1, 3, "up"); }
+	function createXD(color) { return createXCustom(3, color, 1, 3, "down"); }
+	function createXL(color) { return createXCustom(9, color, 3, 3, "left"); }
+	function createXR(color) { return createXCustom(9, color, 3, 3, "right"); }
+	function createXF(color) { return createXCustom(3, color, 3, 1, "front"); }
+	function createXB(color) { return createXCustom(3, color, 3, 1, "back"); }
 
-	function createXD(color) {
-		let children = Array.from({length: 3}, () => create("div", `tile ${color}`));
-		return create("div", "tilegroup tilegroup-1-3 x-down", children);
+	function createXCustom(ntiles, color, nrows, ncolums, position) {
+		let children = Array.from({length: ntiles}, () => create("div", `tile ${color}`));
+		return create("div", `tilegroup tilegroup-${nrows}-${ncolums} x-${position}`, children)
 	}
-
-	function createXL(color) {
-		let children = Array.from({length: 9}, () => create("div", `tile ${color}`));
-		return create("div", "tilegroup tilegroup-3-3 x-left", children);
-	}
-
-	function createXR(color) {
-		let children = Array.from({length: 9}, () => create("div", `tile ${color}`));
-		return create("div", "tilegroup tilegroup-3-3 x-right", children);
-	}
-
-	function createXF(color) {
-		let children = Array.from({length: 3}, () => create("div", `tile ${color}`));
-		return create("div", "tilegroup tilegroup-3-1 x-front", children);
-	}
-
-	function createXB(color) {
-		let children = Array.from({length: 3}, () => create("div", `tile ${color}`));
-		return create("div", "tilegroup tilegroup-3-1 x-back", children);
-	}
-
 }
 
-window.onload = () => {
-	let colors = {
-		U: "white",
-		D: "yellow",
-		L: "orange",
-		R: "red",
-		F: "green",
-		B: "blue"
-	}
+function populateLayerY(colors) {
+	getElementsByClass("layerY").forEach((layer,i) => {
+		let yU = createYU(i == 0 ? colors.U : colors.black);
+		let yD = createYD(i == 2 ? colors.D : colors.black);
+		let yL = createYL(colors.L);
+		let yR = createYR(colors.R);
+		let yF = createYF(colors.F);
+		let yB = createYB(colors.B);
 
-	populateLayerX(colors);
+		[yU,yD,yL,yR,yF,yB].forEach(e => layer.appendChild(e))
+	})
+
+	function createYU(color) { return createYCustom(9, color, 3, 3, "up"); }
+	function createYD(color) { return createYCustom(9, color, 3, 3, "down"); }
+	function createYL(color) { return createYCustom(3, color, 3, 1, "left"); }
+	function createYR(color) { return createYCustom(3, color, 3, 1, "right"); }
+	function createYF(color) { return createYCustom(3, color, 1, 3, "front"); }
+	function createYB(color) { return createYCustom(3, color, 1, 3, "back"); }
+	
+	function createYCustom(ntiles, color, nrows, ncolums, position) {
+		let children = Array.from({length: ntiles}, () => create("div", `tile ${color}`));
+		return create("div", `tilegroup tilegroup-${nrows}-${ncolums} y-${position}`, children)
+	}
 }
