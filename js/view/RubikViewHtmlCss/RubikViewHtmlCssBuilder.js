d6ef66b91eacfce705639af3783a2f7578001218
Víctor Macías <victor.maciaspalla@soprasteria.com>
2018-04-26 17:23:01 +0200

    [WIP] feat: generators for tiles identification

--- js/view/RubikViewHtmlCss/RubikViewHtmlCssBuilder.js
+++ js/view/RubikViewHtmlCss/RubikViewHtmlCssBuilder.js
@@ -1,7 +1,7 @@
 import dom from "/js/dom-operations.js"
 
 export default class RubikViewHtmlCssBuilder {
-	constructor() {}
+	constructor() { }
 
 	build() {
 		this._buildRotationLayersX();
@@ -40,125 +40,128 @@ export default class RubikViewHtmlCssBuilder {
 	_buildRotationLayerZ2() { this._buildRotationLayer("Z", 2, this._facesZ2); }
 
 	_buildRotationLayer(axis, position, children) {
-		let layer = dom.createTagClass("div", `layer, layer${axis} layer${position}`, children);
+		let layer = dom.createTagClass("div", `layer layer${axis} layer${position}`, children);
 		dom.append("rubik", layer);
 	}
 
-	get _facesX0() {
+	get _facesX0() { return this._facesX(0); }
+	get _facesX1() { return this._facesX(1); }
+	get _facesX2() { return this._facesX(2); }
+
+	get _facesY0() { return this._facesY(0); }
+	get _facesY1() { return this._facesY(1); }
+	get _facesY2() { return this._facesY(2); }
+
+	get _facesZ0() { return this._facesZ(0); }
+	get _facesZ1() { return this._facesZ(1); }
+	get _facesZ2() { return this._facesZ(2); }
+
+	_facesX(x) {
+		let l_attr = x == 0 ? [9, 3, 3] : [1, 1, 1];
+		let r_attr = x == 2 ? [9, 3, 3] : [1, 1, 1];
 		return [
-			this._customFaceX(3, 1, 3, 0, "up"),
-			this._customFaceX(3, 1, 3, 0, "down"),
-			this._customFaceX(9, 3, 3, 0, "left"),
-			this._customFaceX(1, 1, 1, 0, "right"),
-			this._customFaceX(3, 3, 1, 0, "front"),
-			this._customFaceX(3, 3, 1, 0, "back")
+			this._customFaceX(l_attr[1], l_attr[2], "left",  this._tilesFromXLeft(x, l_attr[0])),
+			this._customFaceX(r_attr[1], r_attr[2], "right", this._tilesFromXRight(x, r_attr[0])),
+			this._customFaceX(1,         3,         "up",    this._tilesFromXUp(x, 3)),
+			this._customFaceX(1,         3,         "down",  this._tilesFromXDown(x, 3)),
+			this._customFaceX(3,         1,         "front", this._tilesFromXFront(x, 3)),
+			this._customFaceX(3,         1,         "back",  this._tilesFromXBack(x, 3))
 		];
 	}
 
-	get _facesX1() {
+	_facesY(y) {
+		let u_attr = y == 0 ? [9, 3, 3] : [1, 1, 1];
+		let d_attr = y == 2 ? [9, 3, 3] : [1, 1, 1];
 		return [
-			this._customFaceX(3, 1, 3, 1, "up"),
-			this._customFaceX(3, 1, 3, 1, "down"),
-			this._customFaceX(1, 1, 1, 1, "left"),
-			this._customFaceX(1, 1, 1, 1, "right"),
-			this._customFaceX(3, 3, 1, 1, "front"),
-			this._customFaceX(3, 3, 1, 1, "back")
+			this._customFaceY(u_attr[1], u_attr[2], "up",    this._tilesFromYUp(y, u_attr[0])),
+			this._customFaceY(d_attr[1], d_attr[2], "down",  this._tilesFromYDown(y, d_attr[0])),
+			this._customFaceY(3,         1,         "left",  this._tilesFromYLeft(y, 3)),
+			this._customFaceY(3,         1,         "right", this._tilesFromYRight(y, 3)),
+			this._customFaceY(1,         3,         "front", this._tilesFromYFront(y, 3)),
+			this._customFaceY(1,         3,         "back",  this._tilesFromYBack(y, 3))
 		];
 	}
 
-	get _facesX2() {
+	_facesZ(z) {
+		let f_attr = z == 0 ? [9, 3, 3] : [1, 1, 1];
+		let b_attr = z == 2 ? [9, 3, 3] : [1, 1, 1];
 		return [
-			this._customFaceX(3, 1, 3, 2, "up"),
-			this._customFaceX(3, 1, 3, 2, "down"),
-			this._customFaceX(1, 1, 1, 2, "left"),
-			this._customFaceX(9, 3, 3, 2, "right"),
-			this._customFaceX(3, 3, 1, 2, "front"),
-			this._customFaceX(3, 3, 1, 2, "back")
+			this._customFaceZ(f_attr[1], f_attr[2], "front", this._tilesFromZFront(z, f_attr[0])),
+			this._customFaceZ(b_attr[1], b_attr[2], "back",  this._tilesFromZBack(z, b_attr[0])),
+			this._customFaceZ(1,         3,         "up",    this._tilesFromZUp(z, 3)),
+			this._customFaceZ(1,         3,         "down",  this._tilesFromZDown(z, 3)),
+			this._customFaceZ(3,         1,         "left",  this._tilesFromZLeft(z, 3)),
+			this._customFaceZ(3,         1,         "right", this._tilesFromZRight(z, 3))
 		];
 	}
 
-	get _facesY0() {
-		return [
-			this._customFaceY(9, 3, 3, 0, "up"),
-			this._customFaceY(1, 1, 1, 0, "down"),
-			this._customFaceY(3, 3, 1, 0, "left"),
-			this._customFaceY(3, 3, 1, 0, "right"),
-			this._customFaceY(3, 1, 3, 0, "front"),
-			this._customFaceY(3, 1, 3, 0, "back")
-		];
-	}
-
-	get _facesY1() {
-		return [
-			this._customFaceY(1, 1, 1, 1, "up"),
-			this._customFaceY(1, 1, 1, 1, "down"),
-			this._customFaceY(3, 3, 1, 1, "left"),
-			this._customFaceY(3, 3, 1, 1, "right"),
-			this._customFaceY(3, 1, 3, 1, "front"),
-			this._customFaceY(3, 1, 3, 1, "back")
-		];
-	}
-
-	get _facesY2() {
-		return [
-			this._customFaceY(1, 1, 1, 2, "up"),
-			this._customFaceY(9, 3, 3, 2, "down"),
-			this._customFaceY(3, 3, 1, 2, "left"),
-			this._customFaceY(3, 3, 1, 2, "right"),
-			this._customFaceY(3, 1, 3, 2, "front"),
-			this._customFaceY(3, 1, 3, 2, "back")
-		];
-	}
-
-	get _facesZ0() {
-		return [
-			this._customFaceZ(3, 1, 3, 0, "up"),
-			this._customFaceZ(3, 1, 3, 0, "down"),
-			this._customFaceZ(3, 3, 1, 0, "left"),
-			this._customFaceZ(3, 3, 1, 0, "right"),
-			this._customFaceZ(9, 3, 3, 0, "front"),
-			this._customFaceZ(1, 1, 1, 0, "back")
-		];
-	}
-
-	get _facesZ1() {
-		return [
-			this._customFaceZ(3, 1, 3, 1, "up"),
-			this._customFaceZ(3, 1, 3, 1, "down"),
-			this._customFaceZ(3, 3, 1, 1, "left"),
-			this._customFaceZ(3, 3, 1, 1, "right"),
-			this._customFaceZ(1, 1, 1, 1, "front"),
-			this._customFaceZ(1, 1, 1, 1, "back")
-		];
-	}
-
-	get _facesZ2() {
-		return [
-			this._customFaceZ(3, 1, 3, 2, "up"),
-			this._customFaceZ(3, 1, 3, 2, "down"),
-			this._customFaceZ(3, 3, 1, 2, "left"),
-			this._customFaceZ(3, 3, 1, 2, "right"),
-			this._customFaceZ(1, 1, 1, 2, "front"),
-			this._customFaceZ(9, 3, 3, 2, "back")
-		];
-	}
-
-	_customFaceX(ntiles, nrows, ncolums, x, orientation) {
-		let children;
+	_customFaceX(nrows, ncolums, orientation, children) {
 		return this._customFaceWithChildren(nrows, ncolums, "x", orientation, children);
 	}
 
-	_customFaceY(ntiles, nrows, ncolums, x, orientation) {
-		let children;
+	_customFaceY(nrows, ncolums, orientation, children) {
 		return this._customFaceWithChildren(nrows, ncolums, "y", orientation, children);
 	}
 
-	_customFaceZ(ntiles, nrows, ncolums, x, orientation) {
-		let children;
+	_customFaceZ(nrows, ncolums, orientation, children) {
 		return this._customFaceWithChildren(nrows, ncolums, "z", orientation, children);
 	}
 
+	_tilesFromCustom(ntiles, xGen, yGen, zGen, orientation) {
+		return Array.from({length:ntiles}, (e,i) => {
+			let x = xGen.next().value;
+			let y = yGen.next().value;
+			let z = zGen.next().value;
+			return dom.createTagClass("div", `tile tile-${x}-${y}-${z}-${orientation}`);
+		});
+	}
+
+	_tilesFromXLeft(x, ntiles)  { return this._tilesFromCustom(ntiles, same(x), same(9), same(9), "left")}
+	_tilesFromXRight(x, ntiles) { return this._tilesFromCustom(ntiles, same(x), same(9), same(9), "right")}
+	_tilesFromXUp(x, ntiles)    { return this._tilesFromCustom(ntiles, same(x), same(9), same(9), "up")}
+	_tilesFromXDown(x, ntiles)  { return this._tilesFromCustom(ntiles, same(x), same(9), same(9), "down")}
+	_tilesFromXFront(x, ntiles) { return this._tilesFromCustom(ntiles, same(x), same(9), same(9), "front")}
+	_tilesFromXBack(x, ntiles)  { return this._tilesFromCustom(ntiles, same(x), same(9), same(9), "back")}
+
+	_tilesFromYUp(y, ntiles)    { return this._tilesFromCustom(ntiles, same(9), same(y), same(9), "up")}
+	_tilesFromYDown(y, ntiles)  { return this._tilesFromCustom(ntiles, same(9), same(y), same(9), "down")}
+	_tilesFromYLeft(y, ntiles)  { return this._tilesFromCustom(ntiles, same(9), same(y), same(9), "left")}
+	_tilesFromYRight(y, ntiles) { return this._tilesFromCustom(ntiles, same(9), same(y), same(9), "right")}
+	_tilesFromYFront(y, ntiles) { return this._tilesFromCustom(ntiles, same(9), same(y), same(9), "front")}
+	_tilesFromYBack(y, ntiles)  { return this._tilesFromCustom(ntiles, same(9), same(y), same(9), "back")}
+
+	_tilesFromZFront(z, ntiles) { return this._tilesFromCustom(ntiles, same(9), same(9), same(z), "front")}
+	_tilesFromZBack(z, ntiles)  { return this._tilesFromCustom(ntiles, same(9), same(9), same(z), "back")}
+	_tilesFromZUp(z, ntiles)    { return this._tilesFromCustom(ntiles, same(9), same(9), same(z), "up")}
+	_tilesFromZDown(z, ntiles)  { return this._tilesFromCustom(ntiles, same(9), same(9), same(z), "down")}
+	_tilesFromZLeft(z, ntiles)  { return this._tilesFromCustom(ntiles, same(9), same(9), same(z), "left")}
+	_tilesFromZRight(z, ntiles) { return this._tilesFromCustom(ntiles, same(9), same(9), same(z), "right")}
+
 	_customFaceWithChildren(nrows, ncolums, axis, orientation, children) {
 		return dom.createTagClass("div", `tilegroup tilegroup-${nrows}-${ncolums} ${axis}-${orientation}`, children);
+	}
+}
+
+function* ascMod(mod) {
+	let i = 0;
+	while (true) {
+		yield i++ % mod;
+	}
+}
+
+function* ascStep(step) {
+	let i = 0;
+	let j = 0;
+	while (++j) {
+		yield i;
+		if (j%3 == 0) {
+			i++;
+		}
+	}
+}
+
+function* same(value) {
+	while(true) {
+		yield value;
 	}
 }
